e94369a73f96715e24868ab5ff2a60d95c49443e
clara <anton.carlgren@hotmail.com>
2023-07-11 19:51:47 +0200

    i cant believe it works

--- nopilot/src/lib.rs
+++ nopilot/src/lib.rs
@@ -4,34 +4,36 @@ extern crate syn;
 use std::env;
 use proc_macro::TokenStream;
 use serde_json::json;
-use reqwest::{ blocking::Client, StatusCode, };
+use reqwest::blocking::Client;
 use syn::{ parse_macro_input, LitStr, __private::Span, };
 
-const API_MAX_TOKENS: u16 = 512;
-const API_ENDPOINT: &str = "https://api/openai.com/v1/completions";
-const API_MODEL: &str = "";
-const API_ENV_KEY: &str = "OPENAI_KEY";
+const MAX_TOKENS: u16 = 256;
+const ENDPOINT: &str = "https://api.openai.com/v1/completions";
+const MODEL: &str = "text-davinci-003";
+const ENV_API_KEY: &str = "OPENAI_KEY";
+const TEMPERATURE: f32 = 0.0;
 
 #[proc_macro]
-pub fn generate_function(description: TokenStream) -> proc_macro::TokenStream {
+pub fn generate(description: TokenStream) -> proc_macro::TokenStream {
     let parsed_description = parse_macro_input!(description as LitStr).value();
 
-    let api_key = match env::var(API_ENV_KEY) {
+    let api_key = match env::var(ENV_API_KEY) {
         Ok(value) => value,
         Err(_) => return syn::Error::new(
             Span::call_site(), 
-            format!("No environment variable could be found for key: '{API_ENV_KEY}'"))
+            format!("No environment variable could be found for key: '{ENV_API_KEY}'"))
                 .into_compile_error()
                 .into() 
     };  
 
     let response = get_function_from_chatgpt(parsed_description, api_key);
+    
     match response {
         Ok(value) => match value.parse() {
             Ok(parsed_value) => parsed_value,
             Err(value) => syn::Error::new(
                 Span::call_site(), 
-                format!("Could not parse API response: '{value}'"))
+                format!("Could not parse response: '{value}'"))
                     .into_compile_error()
                     .into() 
         },
@@ -45,39 +47,33 @@ pub fn generate_function(description: TokenStream) -> proc_macro::TokenStream {
 
 fn get_function_from_chatgpt(description: String, api_key: String) -> Result<String, String> {
     let client = Client::new();
-
     let request = client
-        .post(API_ENDPOINT)
-        .header("Authorization", format!("Bearer {api_key}"))
+        .post(ENDPOINT)
+        .header("Authorization", format!("Bearer {}", api_key))
         .json(&json!({
             "prompt": description,
-            "max_tokens": API_MAX_TOKENS,
-            "model": API_MODEL
+            "max_tokens": MAX_TOKENS,
+            "model": MODEL,
+            "temperature": TEMPERATURE,
         }));
 
     let response = match request.send() {
         Ok(value) => value,
-        Err(error) => return Err(get_error_code_with_default(error))
+        Err(error) => return Err(error.to_string())
     };
 
     let json = match response
         .json::<serde_json::Value>() {
             Ok(output) => output,
-            Err(error) => return Err(get_error_code_with_default(error))
+            Err(_) => return Err("Could not serialize response.".to_string())
         };
    
-    if let Some(parsed_code) = json["choices"][0]["text"].as_str() {
-        return Ok(parsed_code.to_string());
+    match json["choices"][0]["text"].as_str() {
+        Some(parsed_code) => { 
+            println!("Prompt: {}", description); 
+            println!("Code: {}", parsed_code);
+            return Ok(parsed_code.to_string());
+        },
+        None => Err(json["error"]["message"].to_string())
     }
-
-    Err("Invalid API response format.".to_string())
-}
-
-fn get_error_code_with_default(response: reqwest::Error) -> String {
-        let status_code = match response.status() {
-            Some(code) => code,
-            None => StatusCode::INTERNAL_SERVER_ERROR
-        };
-
-        return status_code.as_str().to_string();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use nopilot;
+
+nopilot::generate!("Write a Rust function called 'sqrt', that takes an u32 and returns the square root of that number.");
+nopilot::generate!("Write a Rust function called 'to_char_vec' that takes a &str as an argument, and returns a Vec<&str>, where each item represents a character in the provided argument.");
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(sqrt(144));
+    dbg!(to_char_vec("Hello, world!"));
 }
